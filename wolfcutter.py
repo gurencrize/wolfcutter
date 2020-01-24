b7acdf01a0fcb0ba512439fcf68bdec56fe80def
gurencrize <34015092+gurencrize@users.noreply.github.com>
2020-01-24 09:55:50 +0900

    使ってない関数消してボタンの関数名をわかりやすくした

--- wolfcutter.py
+++ wolfcutter.py
@@ -14,14 +14,6 @@ class griddraw:
     self.img = Image.open(imgpass)
     self.width,self.height = self.img.size
     self.updatelabel1()
-  def create_white(self):
-    # オリジナル画像と同じサイズの画像を生成
-    img_white = Image.new('RGBA',(self.width,self.height))
-    img_white.putalpha(0)
-    draw_white = ImageDraw.Draw(img_white)
-    draw_vertical(tate,draw_white)
-    draw_horizonal(yoko,draw_white)
-    img_white.save('white_grided.png','PNG')
   def imgtrim(self):
     img = Image.open(self.imgpass)
     if self.putpass=="":
@@ -38,7 +30,9 @@ class griddraw:
       for i in range(tate):
         for j in range(yoko):
           trimimg=img.crop((trimwidth*j,trimheight*i,trimwidth*(j+1),trimheight*(i+1)))
+          #上書き確認入れたい
           trimimg.save(dirname+"/"+imgname+str(i*yoko+j+1)+".png")
+      #完了時になんかメッセージ出したい
   def setputpass(self,n):
     self.putpass=n
   def updatelabel1(self):
@@ -47,7 +41,6 @@ class griddraw:
     draw_vertical(int(sptxt1.get()),drawimg)
     draw_horizonal(int(sptxt2.get()),drawimg)
     img.save('original_grided.png','PNG')
-    #画像出力処理ぼ
     photoim=PhotoImage(file='original_grided.png')
     label1.configure(image=photoim)
     label1.image=photoim
@@ -70,7 +63,6 @@ def draw_horizonal(n:int,draw):
     for i in range(1,n):
       a=int(height/n)
       draw.line([(0,a*i),(width,a*i)],fill=(255,0,0),width=1)
-
 root = Tk()
 root.title('補助線描画')
 
@@ -105,39 +97,34 @@ sptxt2.set(yoko)
 spinbox2=Spinbox(frame1,from_=0,to=1000,textvariable=sptxt2,command=redrawline)
 spinbox2.grid(row=1,column=1)
 
-def button1():
+def inputimage():
   fTyp = [("Image Files",('.jpg','.png'))]
   iDir = os.path.abspath(os.path.dirname(__file__))
   imgpath = filedialog.askopenfilename(filetypes = fTyp,initialdir = iDir)
   image.readimg(imgpath)
-button1=ttk.Button(frame1,text="画像を読み込み",command=button1)
+button1=ttk.Button(frame1,text="画像を読み込み",command=inputimage)
 button1.grid(row=1,column=2)
 
-def button2():
-  image.create_white()
-button2=ttk.Button(frame1,text="白紙を生成",command=button2)
-#button2.grid(row=1,column=3)
-
-def button3():#線の設定を上書きするための関数。tateとyokoを変えると好きな本数線を引けてすごい
+def gridreset():#線の設定を上書きするための関数。tateとyokoを変えると好きな本数線を引けてすごい
   tate=0
   yoko=0
   sptxt1.set(tate)
   sptxt2.set(yoko)
   redrawline()
-button3=ttk.Button(frame1,text="リセット",command=button3)
+button3=ttk.Button(frame1,text="リセット",command=gridreset)
 button3.grid(row=3,column=2)
 
-def button4():
+def choosefolder():
   ini_dir = 'C:\\Program Files\\Python37'
   ret = filedialog.askdirectory(initialdir=ini_dir, title='フォルダを選択してください', mustexist = True)
   txt4.set(str(ret))
   image.setputpass(str(ret))
-button4=ttk.Button(frame1,text="保存先フォルダ選択",command=button4)
+button4=ttk.Button(frame1,text="保存先フォルダ選択",command=choosefolder)
 button4.grid(row=2,column=2)
 
-def button5():
+def triming():
   image.imgtrim()
-button5=ttk.Button(frame1,text="連番出力",command=button5)
+button5=ttk.Button(frame1,text="連番出力",command=triming)
 button5.grid(row=3,column=0)
 
 root.mainloop()
